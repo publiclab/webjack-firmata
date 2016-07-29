a7c2b9b5890b1cfafe50168bd2477dcdd1766001
Richard Meister <richard.meister.de@gmail.com>
2016-07-29 19:49:36 +0200

    initial, not functional version

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# webjack-firmata
+WebJackPort
+====
+
+...is a wrapper for [WebJack](https://github.com/publiclab/webjack) to use as transport layer for [firmata.js](https://github.com/firmata/firmata.js). WebJackPort is inspired by [EtherPort](https://github.com/rwaldron/etherport) from Rick Waldron.
+
+Try it out in this live demo: https://publiclab.github.io/webjack-firmata/examples/
+
+## Install
+```
+npm install --save webjackport
+```
+
+
+## Use
+```js
+var Firmata = require("firmata").Board;
+var WebJackPort = require("webjackport");
+var board = new Firmata(new WebJackPort());
+
+board.on("ready", function() {
+  console.log("READY!");
+  console.log(
+    board.firmware.name + "-" +
+    board.firmware.version.major + "." +
+    board.firmware.version.minor
+  );
+
+  var state = 1;
+
+  this.pinMode(13, this.MODES.OUTPUT);
+
+  setInterval(function() {
+    this.digitalWrite(13, (state ^= 1));
+  }.bind(this), 500);
+});
+```
+
+
+
+
+## Building
+
+webjackport.js is built using a Grunt task from the source files in `/src/`, and the compiled file is saved to `/dist/webjackport.js`. To build, run `grunt build`. To watch files for changes, and build whenever they occur, run `grunt`. 
+
+## Building the demo
+
+The demo is bundled with [webpack](https://webpack.github.io/). Before executing `webpack`, do `npm install`.
---
+++ src/index.js
@@ -0,0 +1,67 @@
+/**
+ * Global Environment Dependencies
+ */
+/* jshint -W079 */
+if (!Object.assign || !Map) {
+  require("es6-shim");
+}
+
+var WebJack = require("webjack");
+var Emitter = require("events").EventEmitter;
+var priv = new Map();
+
+function WebJackPort() {
+  Emitter.call(this);
+
+
+  // Alias used in state.flushTo
+  var WebJackPort = this;
+
+  this.name = "WebJackPort";
+
+  var state = {
+    queue: [],
+    socket: null,
+    flushTo: function(socket) {
+      if (this.socket === null) {
+        this.socket = socket;
+        WebJackPort.emit("open");
+      }
+      if (this.queue.length) {
+        this.queue.forEach(function(buffer) {
+          this.socket.write(buffer);
+        }, this);
+
+        this.queue.length = 0;
+      }
+    }
+  };
+
+  var connection = new WebJack.Connection({firmata : true});
+  state.flushTo(connection);
+
+  connection.listen(function(data) {
+  	console.log(data);
+  	WebJackPort.emit("data", data)
+  });
+
+  priv.set(this, state);
+}
+
+WebJackPort.prototype = Object.create(Emitter.prototype, {
+  constructor: {
+    value: WebJackPort
+  }
+});
+
+WebJackPort.prototype.write = function(buffer) {
+  var state = priv.get(this);
+
+  if (state.socket === null)
+    state.queue.push(buffer);
+  else 
+    state.socket.send(buffer);
+};
+
+module.exports = WebJackPort;
+
