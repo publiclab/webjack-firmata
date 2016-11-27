2bc977d88f024a5f09a1997ef3da71aebb37dd01
jywarren <jeff@unterbahn.com>
2016-11-27 11:56:38 -0500

    submit listeners for serial input

--- example/serial/serial.js
+++ example/serial/serial.js
@@ -1,4 +1,4 @@
-(function() {
+$(document).ready(function() {
 
   Demo.board.serialConfig({
     portId: Demo.board.SERIAL_PORT_IDs.HW_SERIAL0,
@@ -9,13 +9,18 @@
     //  [SW Serial only] The TX pin of the SoftwareSerial instance
   });
 
-  $('.serialWriteSend').on('click', function serialWrite() {
+  $('.serialWriteSend').click(serialWrite);
+  $('.serial').submit(serialWrite);
+
+  function serialWrite(e) {
 
     var text = $('.serialWrite').val();
 
     console.log('SerialWrite: ' + text);
     Demo.board.serialWrite(0, text); // port, bytesIn
 
-  });
+    return false;
 
-})();
+  }
+
+});
